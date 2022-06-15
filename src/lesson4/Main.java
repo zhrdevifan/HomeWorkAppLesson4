374c8669a7733be693c463de128f61cf54a81bd5
zhrdv <helouword@gmail.com>
2022-06-16 01:05:15 +0300

    домашка 4 наконецто что то из меня вышло

--- src/lesson4/Main.java
+++ src/lesson4/Main.java
@@ -1,133 +1,156 @@
 package lesson4;
 
-
-
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
-        String[][] field = initField(3);
+        String [][] field = initField(3);
         printField(field);
 
         while (true) {
-            //ход игрока, проверка на ничью, проверка на победителя
-            humanTurn(field);
-            printField(field);
-            if (isDraw(field)) {
-                break;
-            }
-            if (isWinner(field, "X")) {
-                System.out.println("Вы победили :)");
-                break;
-            }
 
-            //ход ии, проверка на ничью, проверка на победителя
-            aiTurn(field);
-            printField(field);
-            if (isDraw(field)) {
-                break;
-            }
-            if (isWinner(field, "0")) {
-                System.out.println("Искусственный интеллект одержал победу. Скайнет близко :|");
-                break;
-            }
+        HumanTurn(field);
+        printField(field);
+       if (isDraw(field)) {
+       break;
+       }
+       if (isWinner(field,"X")){
+           System.out.println("Вы победили");
+           break;
+       }
 
+        AITurn(field);
+        printField(field);
+        if (isDraw(field)){
+            break;
+    }  if (isWinner(field, "0")){
+        System.out.println("Вы проиграли");
+        break;
+            }
         }
-        System.out.println("Игра закончена");
-    }
 
-    public static String[][] initField(int size) {
-        String[][] field = new String[size][size];
-        for (int i = 0; i < field.length; i++) {
-            for (int j = 0; j < field[0].length; j++) {
-                field[i][j] = "*";
+        System.out.println("Game Over");
+
+    }
+//тут мы обявляем поле и присвиваем значения звездочек
+    public static String [][] initField( int size) {
+        String [][] field = new String[size][size];
+        for (int i = 0; i<field.length; i++){
+            for (int j = 0; j < field[0].length; j++){ //почему в данном цикле используется
+                 field[i][j] = "*";                     // field[0] ???
             }
         }
         return field;
     }
 
-    public static void printField(String[][] field) {
-        for (int i = 0; i < field.length; i++) {
-            for (int j = 0; j < field[0].length; j++) {
-                System.out.print(field[i][j] + " ");
-            }
-            System.out.println();
+ // тут мы выводим значение поля в консоль
+    public static void printField(String [] [] field) {
+        for (int i = 0; i<field.length; i++){
+            for (int j = 0; j < field[0].length; j++){
+                System.out.print(field[i][j]+ "  ");
+
+            }System.out.println();
         }
-        System.out.println("-----");
+
     }
 
-    public static void humanTurn(String[][] field) {
+    // тут мы делаем вызов ввода в консоль значений
+    public static void HumanTurn(String [] [] field) {
         Scanner scanner = new Scanner(System.in);
         while (true) {
-            System.out.println("Укажите клетку по x и y: ");
-            int x = scanner.nextInt()-1;
-            int y = scanner.nextInt()-1;
+            System.out.println("Введите Х и У");
+            int x = scanner.nextInt() - 1;
+            int y = scanner.nextInt() - 1;
             int size = field.length;
 
             if (x < size && y < size && x >= 0 && y >= 0) {
-
                 if (field[x][y].equals("*")) {
                     field[x][y] = "X";
                     break;
                 } else {
-                    System.out.println("Вы сходили не туда");
+                    System.out.println(" поле занято!");
                 }
-
             } else {
-                System.out.println("Вы тыкнули не игровое поле");
+                System.out.println("Вы попали мимо поля");
             }
-
         }
     }
 
-    public static void aiTurn(String[][] field) {
+// тут  ход искуственного интелекта
+    public static void AITurn(String [] [] field) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("==============");
         while (true) {
             Random random = new Random();
             int x = random.nextInt(field.length);
             int y = random.nextInt(field.length);
+            //int size = field.length;
             if (field[x][y].equals("*")) {
                 field[x][y] = "0";
-                break;
-            }
+                    break;
+                }
         }
-        System.out.println("Комп сходил B)");
+        System.out.println("Компьютер сделал свой выбор");
     }
 
-    public static boolean isDraw(String[][] field) {
-        for (int i = 0; i < field.length; i++) {
-            for (int j = 0; j < field[0].length; j++) {
-                if (field[i][j].equals("*")) {
+    //Тут производится проверка на ничью
+
+    public static boolean isDraw(String [] [] field) {
+
+        for (int i = 0; i<field.length; i++){
+            for (int j = 0; j < field[0].length; j++){
+                if (field[i][j].equals("*")){
                     return false;
                 }
             }
         }
-        return true;
+               return true;
+
     }
 
-    public static boolean isWinner(String[][] field, String turn) {
 
-        if (field[0][0].equals(turn) && field[0][1].equals(turn) && field[0][2].equals(turn) ||
-                field[1][0].equals(turn) && field[1][1].equals(turn) && field[1][2].equals(turn) ||
-                field[2][0].equals(turn) && field[2][1].equals(turn) && field[2][2].equals(turn)
-        ) {
-            return true;
-        }
-        //вертикаль
-        else if (field[0][0].equals(turn) && field[1][0].equals(turn) && field[2][0].equals(turn) ||
-                field[0][1].equals(turn) && field[1][1].equals(turn) && field[2][1].equals(turn) ||
-                field[0][2].equals(turn) && field[1][2].equals(turn) && field[2][2].equals(turn)) {
-            return true;
-        }
-        //диагональ
-        else if (field[0][0].equals(turn) && field[1][1].equals(turn) && field[2][2].equals(turn) ||
-                field[2][0].equals(turn) && field[1][1].equals(turn) && field[0][2].equals(turn)) {
-            return true;
-        }
+    public static boolean isWinner(String[][] field, String dot) {
 
+        for (int i = 0; i < 3; i++)
+            if ((field[i][0] == dot && field[i][1] == dot &&
+                    field[i][2] == dot) ||
+                    (field[0][i] == dot && field[1][i] == dot &&
+                            field[2][i] == dot))
+                return true;
+        if ((field[0][0] == dot && field[1][1] == dot &&
+                field[2][2] == dot) ||
+                (field[2][0] == dot && field[1][1] == dot &&
+                        field[0][2] == dot))
+            return true;
         return false;
     }
 
-}
+       // if (field[0][0].equals(turn) && field[0][1].equals(turn) && field[0][2].equals(turn) ||
+          //      field[1][0].equals(turn) && field[1][1].equals(turn) && field[1][2].equals(turn) ||
+           //     field[2][0].equals(turn) && field[2][1].equals(turn) && field[2][2].equals(turn)
+      //  ) {
+       //     return true;
+       // }
+        //вертикаль
+       // else if (field[0][0].equals(turn) && field[1][0].equals(turn) && field[2][0].equals(turn) ||
+      //          field[0][1].equals(turn) && field[1][1].equals(turn) && field[2][1].equals(turn) ||
+        //        field[0][2].equals(turn) && field[1][2].equals(turn) && field[2][2].equals(turn)) {
+        //    return true;
+        }
+        //диагональ
+       // else if (field[0][0].equals(turn) && field[1][1].equals(turn) && field[2][2].equals(turn) ||
+       //         field[2][0].equals(turn) && field[1][1].equals(turn) && field[0][2].equals(turn)) {
+       //     return true;
+      //  }
+
+      //  return false;
+    //}
+
+
+
+
+
+
+//}
+
